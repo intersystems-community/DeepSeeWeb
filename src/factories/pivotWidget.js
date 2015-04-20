d379f0cbfd6e0fb51fe8d9b3a5770e768dc7a7b1
Gnibeda <anton.donnu@gmail.com>
2015-04-20 12:35:24 +0300

    new widget

---
+++ src/factories/pivotWidget.js
@@ -0,0 +1,36 @@
+(function() {
+    'use strict';
+
+    function PivotWidgetFact(BaseChart) {
+
+        function PivotWidget($scope) {
+            var _this = this;
+            this.lpt = undefined;
+            this.onInit = onInit;
+            this.requestData = requestData;
+            this.onResize = onResize;
+
+            function onInit(lpt) {
+                _this.lpt = lpt;
+            }
+
+            function requestData() {
+                if (_this.lpt) {
+                    var newMdx = _this.getMDX();
+                    if (_this.lpt.getActualMDX() != newMdx) _this.lpt.changeBasicMDX(newMdx);
+                    _this.lpt.refresh();
+                }
+            }
+
+            function onResize() {
+                if (_this.lpt) _this.lpt.updateSizes();
+            }
+        }
+
+        return PivotWidget;
+    }
+
+    angular.module('widgets')
+        .factory('PivotWidget', ['BaseChart', PivotWidgetFact]);
+
+})();
