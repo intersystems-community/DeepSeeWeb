93a26ff047b2dd55def87aa42406e90441cb9ce6
Gnibeda <anton.donnu@gmail.com>
2015-04-20 12:38:35 +0300

    config changed

--- src/app.js
+++ src/app.js
@@ -5,9 +5,13 @@
     angular.module('widgets', []);
     angular.module('templates', []);
 
-    angular.module('app', ['ngRoute', 'gridster', 'highcharts-ng', 'dashboard', 'widgets', 'templates'])
+    angular.module('app', ['ngRoute', 'ngCookies', 'cgNotify', 'gridster', 'highcharts-ng', 'ng-context-menu', 'ngDialog', 'dashboard', 'widgets', 'templates'])
 
-    .constant('CONST', {})
+    .constant('CONST', {
+        timeout: 10000,
+        ver: "0.3a",
+        hideFolders: localStorage.hideFolders == "true" ? true : false
+    })
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
@@ -30,21 +34,70 @@
             .otherwise({ redirectTo: '/' });
     }])
 
-    .run(['Connector', 'gridsterConfig', 'Lang', start]);
+    .run(['gridsterConfig', 'Lang', '$rootScope', '$window', start]);
 
-    function start(Connector, gridsterConfig, Lang) {
-        gridsterConfig.draggable.handle = ".bookCoverHeader";
-        gridsterConfig.resizable.handles = ['e', 's', 'w', 'ne', 'se', 'sw', 'nw'];
-        gridsterConfig.defaultSizeX = 1;
-        gridsterConfig.defaultSizeY = 1;
+    function start(gridsterConfig, Lang, $rootScope, $window) {
+        if (!localStorage.cleared) {
+            localStorage.clear();
+            localStorage.cleared = true;
+        }
 
+        gridsterConfig.draggable.handle = ".widget-title";
+        gridsterConfig.resizable.handles = ['se'];
+        gridsterConfig.columns = 12;
+        gridsterConfig.floating = true;
+        gridsterConfig.pushing = true;
+        gridsterConfig.defaultSizeX = 2;
+        gridsterConfig.defaultSizeY = 2;
+        gridsterConfig.isResizing = false;
+        gridsterConfig.isDragging = false;
+
+        gridsterConfig.resizable.start = function() {
+            gridsterConfig.isResizing = true;
+        };
+        gridsterConfig.resizable.stop = function() {
+            gridsterConfig.isResizing = false;
+        };
+        gridsterConfig.draggable.start = function() {
+            gridsterConfig.isDragging = true;
+        };
+        gridsterConfig.draggable.stop = function() {
+            gridsterConfig.isDragging = false;
+        };
+
+        /*
+        gridsterConfig.resizable.resize = function(a, b, w) {
+            $rootScope.$broadcast("resizeWidget" + w.$$hashKey);
+        };*/
+
+        //gridsterConfig.itemSize = ($window.innerWidth - 80) / 12;
+        /*$window.onresize = function() {
+            gridsterConfig.itemSize = ($window.innerWidth - 80) / 12;
+            $rootScope.$broadcast("resize");
+        };*/
+
+        // TODO: add lang support
         Highcharts.setOptions({
             lang: {
-                loading: Lang.get("loading")
+                loading: Lang.get("loading"),
+                //months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+                //weekdays: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+                shortMonths: Lang.get("shortMonths")
+                /*exportButtonTitle: "Exportar",
+                printButtonTitle: "Imprimir",
+                rangeSelectorFrom: "De",
+                rangeSelectorTo: "Até",
+                rangeSelectorZoom: "Periodo",
+                downloadPNG: 'Download imagem PNG',
+                downloadJPEG: 'Download imagem JPEG',
+                downloadPDF: 'Download documento PDF',
+                downloadSVG: 'Download imagem SVG'*/
+                // resetZoom: "Reset",
+                // resetZoomTitle: "Reset,
+                // thousandsSep: ".",
+                // decimalPoint: ','
             }
         });
-
-        Connector.checkSigned();
     }
 
 })();
