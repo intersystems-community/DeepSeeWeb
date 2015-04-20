01ef4d397ac61f689439d103c4b5c554a93463d4
Gnibeda <anton.donnu@gmail.com>
2015-04-20 12:33:04 +0300

    added class

--- src/services/typeMap.js
+++ src/services/typeMap.js
@@ -1,24 +1,66 @@
 (function() {
     'use strict';
 
-    function TypeMapSvc(AreaChart, BarChart, LineChart, ColumnChart, PieChart, XyChart, TimeChart) {
+    function TypeMapSvc(AreaChart, BarChart, LineChart, ColumnChart, PieChart, XyChart, TimeChart, PivotWidget, TextWidget) {
         var types = {
-            piechart: PieChart,
-            areachart: AreaChart,
-            barchart: BarChart,
-            linechart: LineChart,
-            combochart: LineChart,
-            columnchart: ColumnChart,
-            xychart: XyChart,
-            timechart: TimeChart
+            piechart: {
+                class: PieChart,
+                type: "chart"
+            },
+            areachart: {
+                class: AreaChart,
+                type: "chart"
+            },
+            barchart: {
+                class: BarChart,
+                type: "chart"
+            },
+            barchartstacked: {
+                class: BarChart,
+                type: "chart"
+            },
+            linechart: {
+                class: LineChart,
+                type: "chart"
+            },
+            combochart: {
+                class: LineChart,
+                type: "chart"
+            },
+            columnchart: {
+                class: ColumnChart,
+                type: "chart"
+            },
+            xychart: {
+                class: XyChart,
+                type: "chart"
+            },
+            timechart: {
+                class: TimeChart,
+                type: "chart"
+            },
+            pivot: {
+                class: PivotWidget,
+                type: "pivot"
+            },
+            textmeter: {
+                class: TextWidget,
+                type: "text"
+            }
         };
 
         this.getClass = function(name) {
-            return types[name.toLowerCase()];
+            if (!types[name.toLowerCase()]) return undefined;
+            return types[name.toLowerCase()].class;
+        };
+
+        this.getType = function(name) {
+            if (!types[name.toLowerCase()]) return undefined;
+            return types[name.toLowerCase()].type;
         };
     }
 
     angular.module('widgets')
-        .service('TypeMap', ['AreaChart', 'BarChart', 'LineChart', 'ColumnChart', 'PieChart', 'XyChart', 'TimeChart', TypeMapSvc]);
+        .service('TypeMap', ['AreaChart', 'BarChart', 'LineChart', 'ColumnChart', 'PieChart', 'XyChart', 'TimeChart', 'PivotWidget', 'TextWidget', TypeMapSvc]);
 
 })();
