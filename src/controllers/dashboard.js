ccaf5431e2476aad0f41f4df9cb4f5599b5a8aea
Gnibeda <anton.donnu@gmail.com>
2015-04-20 12:38:25 +0300

    new widget support and fixes

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -1,48 +1,90 @@
 (function(){
     'use strict';
 
-    function DashboardCtrl($scope, $routeParams, Connector) {
+    function DashboardCtrl($scope, $routeParams, Connector, Error, Filters, Lang) {
+        var _this = this;
+        this.desc = []; // stores widget definition received from server
+        this.ctxItem = undefined;
         $scope.model = {
-            items: [],
-            path: $routeParams.path
+            items: []
         };
+        $scope.getDesc = getDesc;
+        $scope.onCtxMenuShow = onCtxMenuShow;
+        $scope.refreshItem = refreshItem;
+        $scope.setType = setType;
+        $scope.$on("resetWidgets", function() { window.location.reload(); });
 
-        var controllerMap = {
-            //areachart: "highCharts",
-            //pivot: "pivot"
-        };
+        function setType(t) {
+            if (!_this.ctxItem) return;
+            $scope.$broadcast('setType:' + _this.ctxItem, t);
+        }
 
-        /*
-        for (var i = 0; i < 10; i++) {
-            $scope.model.items.push({title: i.toString()});
-        }*/
+        function onCtxMenuShow(item) {
+            _this.ctxItem = item.$$hashKey;
+        }
 
-        //console.log(Connector.getWidgets());
+        function refreshItem() {
+            if (!_this.ctxItem) return;
+            $scope.$broadcast('refresh:' + _this.ctxItem);
+            _this.ctxItem = undefined;
+        }
 
-
-        this.retrieveData = function(result) {
-            for (var i = 0; i < result.children.length; i++) {
-                /*var item = {};
-                item.title = result.children[i].title;
-
-                var ctrl = controllerMap[result.children[i].type.toLowerCase()];
-                if (!ctrl) ctrl = angular.module('widgets').controller('unsupported');
-                item.controller = ctrl;*/
-                var item = {};
-                for (var attr in result.children[i]) { item[attr] = result.children[i][attr]; }
-                $scope.model.items.push(item);
+        function retrieveData(result) {
+            if (!result) return;
+            if (result.Error) {
+                Error.show(result.Error);
+                return;
             }
-        };
+            if (!result.widgets) {
+                Error.show(Lang.get('errNoWidgets'));
+                return;
+            }
+            if (result.filters) Filters.init(result.filters);
 
-        this.onRequestError = function(e, status) {
-            console.error("error: " + e.toString() + ", " + status);
-        };
+            $scope.model.items = [];
+            _this.desc = [];
+            for (var i = 0; i < result.widgets.length; i++) {
+                // Create item for model
+                var item = {
+                    idx: i,
+                    sizeX: 2,
+                    sizeY: 2,
+                    row: parseInt(i / 6) * 2,
+                    col: (i * 2) % 12,
+                    title: result.widgets[i].title,
+                    toolbar: true
+                };
+                if (result.widgets[i].key) setWidgetSizeAndPos(item, result.widgets[i].key.toString());
+                /*if (localStorage["pos" + result.widgets[i].key + "x"] === undefined) {
+                    if ((item.sizeX != 2) || (item.sizeY != 2)) {
+                        item.row = parseInt(i / 6) * item.sizeX;
+                    }
+                }*/
+                $scope.model.items.push(item);
 
-        Connector.getWidgets($routeParams.path).success(this.retrieveData).error(this.onRequestError);
+                // Create item for description
+                item = {};
+                for (var attr in result.widgets[i]) { item[attr] = result.widgets[i][attr]; }
+                _this.desc.push(item);
+            }
+        }
 
+        function setWidgetSizeAndPos(item, k) {
+            if (localStorage["sizes" + k + "x"])  item.sizeX = parseInt(localStorage["sizes" + k + "x"]);
+            if (localStorage["sizes" + k + "y"])  item.sizeY = parseInt(localStorage["sizes" + k + "y"]);
+            if (localStorage["pos" + k + "x"])  item.col = parseInt(localStorage["pos" + k + "x"]);
+            if (localStorage["pos" + k + "y"])  item.row = parseInt(localStorage["pos" + k + "y"]);
+        }
+
+        function getDesc(idx) {
+            if (!_this.desc[idx]) return undefined;
+            return _this.desc[idx];
+        }
+
+        Connector.getWidgets($routeParams.path).success(retrieveData);
     }
 
     angular.module('dashboard')
-        .controller('dashboard', ['$scope', '$routeParams', 'Connector', DashboardCtrl]);
+        .controller('dashboard', ['$scope', '$routeParams', 'Connector', 'Error', 'Filters', 'Lang', DashboardCtrl]);
 
 })();
