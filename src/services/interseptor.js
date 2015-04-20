e463ccca3ad66f0ce18258a21647ea34bdafd4d8
Gnibeda <anton.donnu@gmail.com>
2015-04-20 12:33:38 +0300

    error handling

--- src/services/interseptor.js
+++ src/services/interseptor.js
@@ -1,31 +1,35 @@
 (function() {
     'use strict';
 
-    function InterceptorSvc() {
+    function InterceptorSvc($location, $routeParams) {
         var svc = {
-            request: request,
+            //request: request,
             responseError: responseError
         };
         return svc;
 
-        function request(config) {
-            //config.headers = { Authorization: Session.auth  };
+        /*function request(config) {
+            config.headers = { Authorization: Session.auth  };
             return config;
-        }
+        }*/
 
-        function responseError(err) {
-            console.error(err);
-            return err;
+        function responseError(e) {
+            if (e.status === 401) {
+                var url = $location.$$url;
+                $location.path("/login").search({from: url});
+                return false;
+            }
+            return e;
         }
     }
 
     function HttpConf($httpProvider) {
         //$httpProvider.defaults.headers['X-Requested-With'] = 'XMLHttpRequest';
-        //$httpProvider.interceptors.push('HttpInterceptor');
+        $httpProvider.interceptors.push('HttpInterceptor');
     }
 
     angular.module('app')
-        .service('HttpInterceptor', [InterceptorSvc])
+        .service('HttpInterceptor', ['$location', '$routeParams', InterceptorSvc])
         .config(['$httpProvider', HttpConf]);
 
 })();
