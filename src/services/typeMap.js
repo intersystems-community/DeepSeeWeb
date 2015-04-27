f022d7c009b1d03922146e83b7bd6cfc8e29e80f
Gnibeda <anton.donnu@gmail.com>
2015-04-27 20:54:55 +0300

    new widget types added

--- src/services/typeMap.js
+++ src/services/typeMap.js
@@ -1,8 +1,31 @@
 (function() {
     'use strict';
 
-    function TypeMapSvc(AreaChart, BarChart, LineChart, ColumnChart, PieChart, XyChart, TimeChart, PivotWidget, TextWidget) {
+    function TypeMapSvc(AreaChart, BarChart, LineChart, ColumnChart, PieChart, XyChart, TimeChart, PivotWidget,
+                        TextWidget, HiLowChart, TreeMapChart, BubbleChart, BullseyeChart, SpeedometerChart,
+                        FuelGaugeChart) {
         var types = {
+            fuelgauge: {
+                class: FuelGaugeChart,
+                type: "chart"
+            }, bullseyechart: {
+                class: BullseyeChart,
+                type: "chart"
+            }, speedometer: {
+                class: SpeedometerChart,
+                type: "chart"
+            }, bubblechart: {
+                class: BubbleChart,
+                type: "chart"
+            },
+            treemapchart: {
+                class: TreeMapChart,
+                type: "chart"
+            },
+            hilowchart: {
+                class: HiLowChart,
+                type: "chart"
+            },
             piechart: {
                 class: PieChart,
                 type: "chart"
@@ -61,6 +84,8 @@
     }
 
     angular.module('widgets')
-        .service('TypeMap', ['AreaChart', 'BarChart', 'LineChart', 'ColumnChart', 'PieChart', 'XyChart', 'TimeChart', 'PivotWidget', 'TextWidget', TypeMapSvc]);
+        .service('TypeMap', ['AreaChart', 'BarChart', 'LineChart', 'ColumnChart', 'PieChart', 'XyChart', 'TimeChart',
+            'PivotWidget', 'TextWidget', 'HiLowChart', 'TreeMapChart', 'BubbleChart', 'BullseyeChart', 'SpeedometerChart',
+            'FuelGaugeChart', TypeMapSvc]);
 
 })();
