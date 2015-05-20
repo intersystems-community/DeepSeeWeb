b56a53fdb577b5421a3a77aad20287d9803a0b46
Gnibeda <anton.donnu@gmail.com>
2015-05-20 21:50:02 +0300

    fixes

--- src/factories/areaChart.js
+++ src/factories/areaChart.js
@@ -14,6 +14,7 @@
                     },
                     area: {
                         //stacking: 'percent',
+                        stacking: 'normal',
                         marker: {
                             enabled: false
                         }
