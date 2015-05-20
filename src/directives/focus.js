5dc4a34245e4ac52f81e1b20460984f112965c9b
Gnibeda <anton.donnu@gmail.com>
2015-05-20 21:51:22 +0300

    new directive blur-on-enter

--- src/directives/focus.js
+++ src/directives/focus.js
@@ -27,6 +27,22 @@
                     });
                 }
             };
+        })
+
+        .directive('blurOnEnter', function() {
+            return {
+                restrict: 'A',
+                link: function ($scope, elem, attrs) {
+
+                    elem.bind('keydown', function (e) {
+                        var code = e.keyCode || e.which;
+                        if (code === 13) {
+                            e.preventDefault();
+                            e.currentTarget.blur();
+                        }
+                    });
+                }
+            };
         });
 
 })();
