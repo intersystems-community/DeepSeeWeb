5f97e1e7372a9c35cc9245cd1a99947bef43c835
Gnibeda <anton.donnu@gmail.com>
2015-06-10 21:22:13 +0300

    new directive ng-enter

--- src/directives/focus.js
+++ src/directives/focus.js
@@ -43,6 +43,20 @@
                     });
                 }
             };
+        })
+
+        .directive('ngEnter', function () {
+            return function (scope, element, attrs) {
+                element.bind("keydown keypress", function (event) {
+                    if (event.which === 13) {
+                        scope.$apply(function () {
+                            scope.$eval(attrs.ngEnter);
+                        });
+
+                        event.preventDefault();
+                    }
+                });
+            };
         });
 
 })();
