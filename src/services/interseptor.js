f2b91160dadaf7e1b17efb4723ab92f2c26968d0
Gnibeda <anton.donnu@gmail.com>
2015-07-09 23:39:11 +0300

    timeout error handling

--- src/services/interseptor.js
+++ src/services/interseptor.js
@@ -1,9 +1,10 @@
 (function() {
     'use strict';
 
-    function InterceptorSvc($location, $routeParams) {
+    function InterceptorSvc($q, $location, $routeParams, Lang) {
         var svc = {
             //request: request,
+           // response: onResponse,
             responseError: responseError
         };
         return svc;
@@ -13,11 +14,20 @@
             return config;
         }*/
 
+        /*function onResponse(response) {
+            var deferred = $q.defer();
+            if (response.data === null) response.data = {Error: "timeout"};
+            deferred.resolve(response);
+
+            return deferred.promise;
+        }*/
+
         function responseError(e) {
             /*if (e.status === 503) {
                 Error.show("503. Service unavailable.");
                 return false;
             }*/
+            if (e.data === null && e.status === 0) e.data = {Error: Lang.get("errTimeout")};
             if (e.status === 401) {
                 var url = $location.$$url;
                 $location.path("/login").search({from: url});
@@ -33,7 +43,7 @@
     }
 
     angular.module('app')
-        .service('HttpInterceptor', ['$location', '$routeParams', InterceptorSvc])
+        .service('HttpInterceptor', ['$q', '$location', '$routeParams', 'Lang', InterceptorSvc])
         .config(['$httpProvider', HttpConf]);
 
 })();
