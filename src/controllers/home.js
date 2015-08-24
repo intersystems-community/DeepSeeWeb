3c18fc284492bc628cc6346fbd6152afc21b3aea
Gnibeda <anton.donnu@gmail.com>
2015-08-25 01:08:58 +0300

    fix: title removed when tile have widget

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -287,7 +287,7 @@
                     if (c.icon !== undefined) dashboards[i].icon = c.icon; else dashboards[i].icon = (dashboards[i].title === "" ? (icons.length - 1) : dashboards[i].isFolder ? 2 : 1);
                     dashboards[i].color = c.color || (dashboards[i].title === "" ? 2 : dashboards[i].isFolder ? 3 : 1);
                     dashboards[i].fontColor = c.fontColor || 0;
-                    dashboards[i].customTitle = c.title || dashboards[i].title;
+                    if (dashboards[i].widget === null) dashboards[i].customTitle = c.title || dashboards[i].title; else dashboards[i].title = "";
                 }
 
                 setupList(dashboards);
@@ -346,7 +346,7 @@
                 if (c.icon !== undefined) dashboards[i].icon = c.icon; else dashboards[i].icon = (dashboards[i].title === "" ? (icons.length - 1) : dashboards[i].isFolder ? 2 : 1);
                 dashboards[i].color = c.color || (dashboards[i].title === "" ? 2 : dashboards[i].isFolder ? 3 : 1);
                 dashboards[i].fontColor = c.fontColor || 0;
-                dashboards[i].customTitle = c.title || dashboards[i].title;
+                if (dashboards[i].widget === null) dashboards[i].customTitle = c.title || dashboards[i].title; else dashboards[i].title = "";
             }
 
             setupList(dashboards);
