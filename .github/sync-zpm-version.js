a8f9ba87576df9ee547daacb45b8e40bd1b5c2e9
gnibeda <a.gnybida@ameria.de>
2021-03-23 19:29:11 +0200

    * added script for automatically update module.xml version * changed action to update version

---
+++ .github/sync-zpm-version.js
@@ -0,0 +1,24 @@
+const pkg = require('./../package.json');
+const fs = require('fs');
+const xml2js = require('xml2js');
+const util = require('util');
+
+const parser = new xml2js.Parser();
+const xmlBuilder = new xml2js.Builder();
+
+const data = fs.readFileSync('./Module.xml');
+
+
+parser.parseString(data, function (err, result) {
+    const ver = result.Export.Document[0].Module[0].Version[0];
+    if (ver === pkg.version) {
+        console.log('Versiona are equal: ' + pkg.version);
+        return;
+    }
+    result.Export.Document[0].Module[0].Version[0] = pkg.version;
+
+    const xml = xmlBuilder.buildObject(result, {xmldec: { 'version': '1.0', 'encoding': 'UTF-8' }});
+
+    console.log('Changing version of Module.xml to: ' + pkg.version);
+    fs.writeFileSync('module.xml', xml);
+});
